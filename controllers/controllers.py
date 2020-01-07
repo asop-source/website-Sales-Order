a8bde23787fc1bd6a5ca5e01db37d699ea4b1bdd
Asop-source <asopkarawang@gmail.com>
2020-01-07 10:11:18 +0700

    Membuat Website Sales Orde

---
+++ controllers/controllers.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from odoo import http
+from odoo.http import request
+import simplejson
+import time
+
+class VitWebsiteSo(http.Controller):
+
+	@http.route('/vit/so',type='http' , auth='public', website=True)
+	def index(self, **kw):
+		sale_orders = request.env['sale.order'].search([])
+		return request.render("vit_website_so.index", {
+			'sale_orders': sale_orders
+			})
+
+
+	# membuat ajax
+	@http.route('/vit/so_ajax',type='http' , auth='public', website=True)
+	def index_ajax(self, **kw):
+		sale_orders = request.env['sale.order'].search([])
+		return request.render("vit_website_so.index_ajax", {
+			})
+
+	@http.route('/vit/load_ajax',type='http' , auth='public', website=True)
+	def load_ajax(self, **kw):
+		sale_orders = request.env['sale.order'].search([])
+		result = {}
+		result ['data'] = []
+		for name in sale_orders:
+			result['data'].append([
+				name.name,
+				name.confirmation_date,
+				name.partner_id.name,
+				name.user_id.name,
+				name.amount_total,
+				name.invoice_status,
+			])
+		return simplejson.dumps(result, default=str)
